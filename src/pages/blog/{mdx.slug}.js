fc2a1060997226ae2ed8a2ecc7294a92557edb31
Juraj Majerik <juro.majerik@gmail.com>
2021-12-06 21:48:23 +0100

    part 6

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../../components/layout'
+
+const BlogPost = ({ data }) => {
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <MDXRenderer>
+        {data.mdx.body}
+      </MDXRenderer>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`
+
+export default BlogPost
