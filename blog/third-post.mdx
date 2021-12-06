2f8ae57cbecc25608e04d0b692d9be194435d782
Juraj Majerik <juro.majerik@gmail.com>
2021-12-06 22:09:57 +0100

    part 7

--- blog/third-post.mdx
+++
@@ -1,6 +0,0 @@
----
-title: "Yet Another Post"
-date: "2021-07-25"
----
-
-Wow look at all this content. How do they do it?
--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,25 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -15,13 +28,21 @@ const BlogPost = ({ data }) => {
 }
 
 export const query = graphql`
-  query ($id: String) {
+  query($id: String) {
     mdx(id: {eq: $id}) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `
