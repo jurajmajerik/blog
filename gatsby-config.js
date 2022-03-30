675fe33d68c1af240da192e129c18c990526c60d
Juraj Majerik <jurajmajerik@Jurajs-MacBook-Pro.local>
2022-03-30 07:56:37 +0200

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,20 @@ module.exports = {
     title: "Juraj Majerik",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-7VD7Q42FL9", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
